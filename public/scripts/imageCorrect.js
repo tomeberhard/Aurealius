0841874e9751065bdb7ce3d834c5721dc4c2968b
Scott K <scott.kirker@gmail.com>
2019-06-10 11:31:15 -0400

    fixed load image to crop images to 1/1 for square divs. changed to more noticeable fabric texture body background

--- public/scripts/imageCorrect.js
+++ public/scripts/imageCorrect.js
@@ -1,12 +1,33 @@
 jQuery(function() {
-	jQuery(".orientation").each(function() {
-		var div = $(this);
-		loadImage(
-		        div.attr("image"),
-		        function (img) { div.append(img); },
-		        {orientation: true}
-		    );
+  jQuery(".orientation").each(function() {
+    var div = $(this);
 
-	});
+		let imageClassList = div.attr("class");
 
-})
+		if(imageClassList.includes("followingPrvw") || imageClassList.includes("userProfilePic")) {
+
+			loadImage(
+				div.attr("image"),
+				function(img) {
+					div.append(img);
+				}, {
+					orientation: true,
+					aspectRatio: 1/1
+				}
+			);
+
+		} else {
+			loadImage(
+	      div.attr("image"),
+	      function(img) {
+	        div.append(img);
+	      }, {
+	        orientation: true,
+	      }
+	    );
+
+		}
+
+  });
+
+});
